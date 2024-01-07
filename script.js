c82687fdc46d9921220dfdc6fe0d61519627fb24
terukirdibintang <155874928+terukirdibintang@users.noreply.github.com>
2024-01-07 15:33:53 +0800

    yeah

---
+++ script.js
@@ -0,0 +1,21 @@
+getBirthdayMessage();
+
+function getBirthdayMessage() {
+    let url = window.location.href;
+    let regexName = new RegExp("name" + "(=([^&#]*))");
+    let resultsName = regexName.exec(url);
+    let regexSeg = new RegExp("seg" + "(=([^&#]*))");
+    let resultsSeg = regexSeg.exec(url);
+
+    if (resultsName == null) {
+        birthdayName = "Nata";
+    } else {
+        birthdayName = decodeURIComponent(resultsName[2]);
+    }
+
+    if (resultsSeg == null) {
+        birthdaySeg = "general";
+    } else {
+        birthdaySeg = decodeURIComponent(resultsSeg[2]);
+    }
+}
